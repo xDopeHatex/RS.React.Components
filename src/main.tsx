9f0f5e87d3d972d3b62ba64e9fa233dc60e08e7a
Ivan Gorokhov <xgorokhovx@gmail.com>
2023-11-12 22:29:14 +0200

    feat: fix styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 import ErrorBoundary from "./Components/ErrorBoundary";
 import { BrowserRouter } from "react-router-dom";
